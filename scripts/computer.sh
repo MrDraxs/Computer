46155a92fa4b6e80e80120283b892ddaa0bb1353
MrDraxs <67723468+MrDraxs@users.noreply.github.com>
2022-11-15 18:01:06 -0300

    Update computer.sh

--- scripts/computer.sh
+++ scripts/computer.sh
@@ -1,42 +1,45 @@
 #!/usr/bin/env bash
 
-if [ "$1" = "reboot" ]; then
-	shift
-	sudo reboot
-elif [ "$1" = "install" ]; then
-	shift
-	if [ -x "$(command -v xbps-install)" ];then
-		sudo xbps-install "$@"
-	elif [ -x "$(command -v pacman)" ];then
-		sudo pacman -S "$@"
-	elif [ -x "$(command -v apt)" ];then
-		sudo apt-get install "$@"
-	fi
-elif [ "$1" = "update" ] || [ "$1" = "upgrade" ]; then
-	shift
-	if [ -x "$(command -v xbps-install)" ];then
-		sudo xbps-install -u "$@"
-	elif [ -x "$(command -v pacman)" ];then
-		sudo pacman -Syu
-	elif [ -x "$(command -v apt)" ];then
-		sudo apt-get upgrade "$@"
-	fi
-elif [ "$1" = "search" ]; then
-	shift
-	if [ -x "$(command -v xbps-install)" ];then
-		sudo xbps-query "$@"
-	elif [ -x "$(command -v pacman)" ];then
-		sudo pacman -Ss "$@"
-	elif [ -x "$(command -v apt)" ];then
-		sudo apt-get search "$@"
-	fi
-elif [ "$1" = "uninstall" ] || [ "$1" = "remove" ]; then
-	shift
-	if [ -x "$(command -v xbps-install)" ];then
-		sudo xbps-remove "$@"
-	elif [ -x "$(command -v pacman)" ];then
-		sudo pacman -Rsc "$@"
-	elif [ -x "$(command -v apt)" ];then
-		sudo apt-get remove "$@"
-	fi
-fi
+case "$1" in
+	"reboot") sudo reboot ;;
+	"install")
+		shift	
+		if [ -x "$(command -v xbps-install)" ];then
+			sudo xbps-install "$@"
+		elif [ -x "$(command -v pacman)" ];then
+			sudo pacman -S "$@"
+		elif [ -x "$(command -v apt)" ];then
+			sudo apt-get install "$@"
+		fi
+	;;
+	"update"|"upgrade") 
+		shift	
+		if [ -x "$(command -v xbps-install)" ];then
+			sudo xbps-install -u "$@"
+		elif [ -x "$(command -v pacman)" ];then
+			sudo pacman -Syu
+		elif [ -x "$(command -v apt)" ];then
+			sudo apt-get upgrade "$@"
+		fi
+ 	;;
+	"search") 
+		shift	
+		if [ -x "$(command -v xbps-install)" ];then
+			sudo xbps-query "$@"
+		elif [ -x "$(command -v pacman)" ];then
+			sudo pacman -Ss "$@"
+		elif [ -x "$(command -v apt)" ];then
+			sudo apt-get search "$@"
+		fi
+ 	;;
+	"uninstall"|"remove") 
+		shift	
+		if [ -x "$(command -v xbps-install)" ];then
+			sudo xbps-remove "$@"
+		elif [ -x "$(command -v pacman)" ];then
+			sudo pacman -Rsc "$@"
+		elif [ -x "$(command -v apt)" ];then
+			sudo apt-get remove "$@"
+		fi
+ 	;;
+esac
